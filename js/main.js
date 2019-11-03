42cb9c647747940652a1308c3479e8360aff87fb
royweb <xytank@gmail.com>
2019-11-03 15:52:30 -0500

    Add a class for UI

--- js/main.js
+++ js/main.js
@@ -6,3 +6,26 @@ class Item{
         this.description = description;
     }
 }
+
+// UI class
+class UI{
+    static displayItems(){
+        // for testing
+        const storedItems = [
+            {
+                name: "Pen",
+                location: "In the kitchen drawer",
+                description: "Blue Pen"
+            },
+            {
+                name: "Candy",
+                location: "In the green shelf",
+                description: "Blue Pen"
+            }
+        ];
+
+        const items = storedItems;
+
+        items.forEach((item) => UI.addItemToList(item));
+    }
+}
